9233b31b4e6f5551e6f931b7898c34e0314fdfe0
Malik H <malik.h@webdevpro.net>
2023-01-24 16:01:44 +0100

    correction

--- jour2/03-select-fin.sql
+++ jour2/03-select-fin.sql
@@ -45,3 +45,36 @@ WHERE `like` >= 0 AND `like` <= 30 ;
 SELECT * 
 FROM articles 
 WHERE `like` BETWEEN 0 AND 30 ;
+
+-- requete 1
+-- afficher tous les articles qui ont été publiés en janvier 2023 
+
+SELECT * 
+FROM articles 
+WHERE dt_publication BETWEEN "2023-01-01" AND "2023-01-31";
+
+SELECT * 
+FROM articles 
+WHERE dt_publication >= "2023-01-01" AND dt_publication <= "2023-01-31";
+
+SELECT * 
+FROM articles 
+WHERE strftime("%m" , dt_publication) = "01" AND strftime("%Y" , dt_publication) = "2023"
+
+SELECT * 
+FROM articles 
+WHERE dt_publication LIKE "%2023-01%" ;
+
+-- requete 2
+-- afficher tous les articles dont l'auteur est soit Pierre soit Alain
+-- afficher les colonnes titre , id , dt_publication
+-- pour la colonne date de publication mettre la date au format anglais 
+-- MM/JJ/AAAA
+-- tirer dans l'ordre alphabétique a => z sur la colonne titre 
+
+SELECT titre , 
+       id , 
+       strftime("%m/%d/%Y" , dt_publication ) AS `date au format anglais`
+FROM articles
+WHERE auteur IN ("Pierre" , "Alain") 
+ORDER BY titre ;
